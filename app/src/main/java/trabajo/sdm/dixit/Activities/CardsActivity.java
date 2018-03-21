300e2f7ee555864be6e20ee4b3761cad4228dfd5
JUAN JOSE GONZALEZ BLESA <juagonbl@alumno.upv.es>
2018-03-21 17:01:25 +0100

    Initial commit

---
+++ app/src/main/java/trabajo/sdm/dixit/Activities/CardsActivity.java
@@ -0,0 +1,15 @@
+package trabajo.sdm.dixit.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import trabajo.sdm.dixit.R;
+
+public class CardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
---
+++ app/src/main/java/trabajo/sdm/dixit/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package trabajo.sdm.dixit.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import trabajo.sdm.dixit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
