11a61aaa4bc58d81127e71a8b0fa45309b241111
arevalo96 <32680792+arevalo96@users.noreply.github.com>
2018-03-24 20:07:08 +0100

    estructura menu hecha, creacion de carias activities y paleta de color de la app elegida

--- app/src/main/java/trabajo/sdm/dixit/Activities/MainActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/MainActivity.java
@@ -1,7 +1,9 @@
 package trabajo.sdm.dixit.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import trabajo.sdm.dixit.R;
 
@@ -12,4 +14,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // This method is executed when any button in the Main screen is clicked
+    public void onClick(View v) {
+
+        // Intent object to launch the related activity
+        Intent intent = null;
+
+        // Determine the activity to launch according to the Id of the Button clicked
+        switch (v.getId()) {
+
+            // Activity for starting the game
+            case R.id.bPlay:
+                intent = new Intent(this, PlayActivity.class);
+                break;
+
+            // Activity for showing score
+            case R.id.bScores:
+                intent = new Intent(this, ScoresActivity.class);
+                break;
+
+            // Activity for configuring the application
+            case R.id.bSettings:
+                intent = new Intent(this, SettingsActivity.class);
+                break;
+
+            case R.id.bInstructions:
+                intent = new Intent(this, RulesActivity.class);
+                break;
+
+            // Activity for showing credits
+            /*case R.id.creditsoption:
+                intent = new Intent(this, SettingsActivity.class);
+                break;
+            */
+        }
+
+        // Launch the required activity
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+
 }
