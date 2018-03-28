d71334782e2953f57860b8fa08fcc7a511ce9d0f
Carlos Herrera <carloshpiedra@gmail.com>
2018-03-28 05:18:33 +0200

    Opción créditos agregada al menú main

--- app/src/main/java/trabajo/sdm/dixit/Activities/MainActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/MainActivity.java
@@ -3,6 +3,8 @@ package trabajo.sdm.dixit.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import trabajo.sdm.dixit.R;
@@ -54,6 +56,27 @@ public class MainActivity extends AppCompatActivity {
         if (intent != null) {
             startActivity(intent);
         }
+    }
+
+    /* This method is executed automatically when the activity starts and it creates an ActionBar */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /* This method is executed when an menu option is clicked */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.creditsoption:
+                Intent intent = new Intent(this, CreditsActivity.class);
+                this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+        return true;
     }
 
 
