cd52b4b524acf9f156153e537bd6165b56ca5f35
Carlos Herrera <carloshpiedra@gmail.com>
2018-04-04 13:10:07 +0200

    Tabhost scores agregado -> Local y Amigos

--- app/src/main/java/trabajo/sdm/dixit/Activities/ScoresActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/ScoresActivity.java
@@ -2,6 +2,7 @@ package trabajo.sdm.dixit.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import trabajo.sdm.dixit.R;
 
@@ -11,5 +12,26 @@ public class ScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+
+        //Tabs Control
+        TabHost host = (TabHost) findViewById(R.id.my_tabhost);
+        host.setup();
+
+        //--- Tab1 ---
+        TabHost.TabSpec spec = host.newTabSpec("mitab1");
+        //Tab Indicator specified as Label and Icon
+        spec.setIndicator("Local", getResources().getDrawable(R.drawable.ic_person_outline));
+        spec.setContent(R.id.tab1);
+        host.addTab(spec);
+
+        //--- Tab2 ---
+        spec = host.newTabSpec("mitab2");
+        // Tab Indicator specified as Label and Icon
+        spec.setIndicator("Friends", getResources().getDrawable(R.drawable.ic_people));
+        spec.setContent(R.id.tab2);
+
+         host.addTab(spec);
+
+
     }
 }
