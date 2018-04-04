482528622de6daff8d801ad165ac604ee0028dc0
Carlos Herrera <carloshpiedra@gmail.com>
2018-04-04 13:57:04 +0200

    Animación agregada al pasar a la siguiente carta en activity Cards

--- app/src/main/java/trabajo/sdm/dixit/Activities/CardsActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/CardsActivity.java
@@ -4,21 +4,28 @@ package trabajo.sdm.dixit.Activities;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 import adapters.ImageAdapter;
 import trabajo.sdm.dixit.R;
 
-public class CardsActivity extends AppCompatActivity {
+public class CardsActivity extends AppCompatActivity{
+
+    private static final float MIN_SCALE = 0.75f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
+        ViewPager viewPager = findViewById(R.id.viewPagerCards);
         ImageAdapter adapter = new ImageAdapter(this);
         viewPager.setAdapter(adapter);
 
+        viewPager.setPageTransformer(true, new DepthPageTransformer());
+
     }
+
+
 }
--- app/src/main/java/trabajo/sdm/dixit/Activities/PlayActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/PlayActivity.java
@@ -1,15 +1,20 @@
 package trabajo.sdm.dixit.Activities;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import adapters.ImageAdapter;
 import trabajo.sdm.dixit.R;
 
 public class PlayActivity extends AppCompatActivity {
+
+    public int[] cards = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
     }
 }
