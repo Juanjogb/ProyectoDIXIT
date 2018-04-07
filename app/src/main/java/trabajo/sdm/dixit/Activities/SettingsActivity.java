6bc093f414225c55812d04d673fa30e65b26fbcb
Juanjo <juanjogb@live.com>
2018-04-08 01:14:42 +0200

    Ultimas cambios, perfilando las activities

--- app/src/main/java/trabajo/sdm/dixit/Activities/SettingsActivity.java
+++ app/src/main/java/trabajo/sdm/dixit/Activities/SettingsActivity.java
@@ -8,10 +8,8 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
 
 import trabajo.sdm.dixit.R;
@@ -19,16 +17,13 @@ import trabajo.sdm.dixit.R;
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageButton avatar = null;
-
+    private int indice;
     static final int ACTION_AVATAR_PHOTO = 1;
     static final int ACTION_AVATAR_GALLERY = 2;
 
     private SharedPreferences preferences = null;
     static final String AVATAR = "avatar";
-    static final String NAME = "name";
-    static final String YEARS = "age";
-    static final String SEX = "sex";
-
+    Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +32,16 @@ public class SettingsActivity extends AppCompatActivity {
         avatar = (ImageButton) findViewById(R.id.butAvatarImage);
         avatar.setOnClickListener(new View.OnClickListener(){
             public void onClick(View arg0) {
-                showPhotoDialog();
+                //showPhotoDialog();
+                intent = new Intent(getApplicationContext(), PhotoSelectorDialog.class);
+                startActivity(intent);
+                getIntent().getIntExtra("num_avatar",indice);
+                //setAvatar(indice);
+                //String aux = "R.drawable.ava"+indice;
+               // Bitmap bmp = BitmapFactory.decodeResource(getResources(), "R.drawable.ava"+indice);
+                //avatar.setImageBitmap(bmp);
+                String uriString = preferences.getString(AVATAR,"android.resource://trabajo.sdm.dixit/R.drawable.ava" + indice);
+                avatar.setImageURI(Uri.parse(uriString));
             }
         });
 
@@ -45,13 +49,17 @@ public class SettingsActivity extends AppCompatActivity {
         preferences = getPreferences(0);
     }
 
-    public void showPhotoDialog(){
-        FragmentManager fm= getSupportFragmentManager();
-        PhotoSelectorDialog avatarDialog = new PhotoSelectorDialog();
-        avatarDialog.show(fm,"photo");
-    }
+    /*public void showPhotoDialog(){
+        Intent avatarSelect = new Intent(getApplicationContext(), PhotoSelectorDialog.class);
+        startActivity(avatarSelect);
+        // FragmentManager fm= getSupportFragmentManager();
+       // PhotoSelectorDialog avatarDialog = new PhotoSelectorDialog();
+        //avatarDialog.show(fm,"photo");
+        getIntent().getIntExtra("num_avatar",indice);
+        setAvatar(indice);
+    }*/
 
-    public void avatarPhoto() {
+    /*public void avatarPhoto() {
         Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(pictureIntent, ACTION_AVATAR_PHOTO);
     }
@@ -61,7 +69,7 @@ public class SettingsActivity extends AppCompatActivity {
         pickPhoto.setType("image/*");
         startActivityForResult(pickPhoto, ACTION_AVATAR_GALLERY);
     }
-
+*/
     protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_CANCELED) {
             //no hacer nada
@@ -90,15 +98,15 @@ public class SettingsActivity extends AppCompatActivity {
         avatar.setImageBitmap(bitmap);
     }
 
-    public void avatarDefault(){
+    public void selectAvatar(int indice){
         SharedPreferences.Editor editor = preferences.edit();
         editor.remove(AVATAR);
         editor.commit();
-        setAvatar();
+        setAvatar(indice);
     }
 
-    private void setAvatar() {
-        String uriString = preferences.getString(AVATAR,"android.resource://trabajo.sdm.dixit/" + R.drawable.ava4);
+    private void setAvatar(int x) {
+        String uriString = preferences.getString(AVATAR,"android.resource://trabajo.sdm.dixit/R.drawable.ava" + x);
         avatar.setImageURI(Uri.parse(uriString));
     }
 
